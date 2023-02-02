766ad452e7a0f299d689847545e350bfe91846a2
Thomas-Basadonne <thommybasa@gmail.com>
2023-02-02 17:19:06 +0100

    bomb class

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 // Consegna
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
 
 /**************************************************
  *                                                *
@@ -10,6 +10,17 @@
  *                                                *
  **************************************************/
 const startGame = document.getElementById("start-game");
+
+// ARRY BOMBE
+let bombe = [];
+while (bombe.length < 16) {
+  bombNumber = Math.floor(Math.random() * 100) + 1;
+
+  if (!bombe.includes(bombNumber)) {
+    bombe.push(bombNumber);
+  }
+}
+console.log(bombe);
 // const startGameSelect = document.getElementById("iniziaSelect");
 
 /**************************************************
@@ -57,8 +68,17 @@ function generaGriglia(grid, gridDimension) {
     // aggiungo classe alla cella
     cella.classList.add("square");
 
+    // cella.addEventListener("click", function () {
+    //   this.classList.toggle("active");
+    //   console.log(this.innerHTML);
+    // });
+    // debugger;
     cella.addEventListener("click", function () {
-      this.classList.toggle("active");
+      if (bombe.includes(this.innerHTML)) {
+        this.classList.toggle("bomb");
+      } else {
+        this.classList.toggle("active");
+      }
       console.log(this.innerHTML);
     });
     grid.append(cella);
